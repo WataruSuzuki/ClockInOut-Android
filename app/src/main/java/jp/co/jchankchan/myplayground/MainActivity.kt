12d88d5e67a6b4f1ffc176f778ff0887de32a7f7
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-07-09 22:10:00 +0900

    Modified dependency : integrated android library

--- app/src/main/java/jp/co/jchankchan/myplayground/MainActivity.kt
+++ app/src/main/java/jp/co/jchankchan/myplayground/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.mylibrary.HelloWorld
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +23,7 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        HelloWorld.printMessage()
     }
 }
---
+++ mylibrary/src/main/java/com/example/mylibrary/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+public class HelloWorld {
+    public static void printMessage() {
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "Hello android library";
+    }
+}
